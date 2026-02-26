fffb5c7c4ca160c5a59b8ca6c8b4d6cd1ce3b8bb
AnhMaiTuan <amaituan@openwt.com>
2026-02-26 14:06:49 +0700

    feat: Mutiple profiles and Fail-fast

---
+++ Webapp.Infra/Configuration/AppSettingsInstaller.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Webapp.Application.Abstractions;
+
+namespace Webapp.Infra.Configuration;
+
+internal class AppSettingsInstaller : IServiceInstaller
+{
+    public void Install(IServiceCollection services, IConfiguration configuration)
+    {
+        var appSettings = new AppSettings();
+        configuration.Bind(appSettings);
+
+        var validator = new AppSettingsValidator();
+        var validationResult = validator.Validate(appSettings);
+
+        if (!validationResult.IsValid)
+        {
+            throw new Exception("Invalid app settings: " + string.Join(", ", validationResult.Errors.Select(e => e.ErrorMessage)));
+        }
+
+        services.AddSingleton(appSettings);
+    }
+}
